47f5775e2a4ac2ba2c2117dadbe775e3bbd34b90
Rokon-Uz-Zaman Roman <mdroman266@gmail.com>
2021-11-13 15:21:06 +0600

    Update parctice1.sql

--- SQL/parctice1.sql
+++ SQL/parctice1.sql
@@ -1,1 +1,7 @@
-SQL
+create table class7(Roll integer , Name varchar(20) );
+
+insert into  class7 values(1, "roman");
+insert into  class7 values(2, "amir");
+insert into  class7 values(3, "rahat");
+
+select * from class7;
